3c5c1c02bac7ff966cc245cfac8aca0c214aaa85
davirk21 <davirk21@gmail.com>
2017-09-08 17:37:53 -0700

    VideoListEntry rendered

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,26 @@
-var App = () => (
+var App = (props) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>search<Search/></em> view goes here</h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer <VideoPlayer /></em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList<VideoList videos = {props.videos} /></em> view goes here</h5></div>
       </div>
     </div>
   </div>
 );
 
+ReactDOM.render(<App videos = {exampleVideoData} />, document.getElementById('app'));
+
+// ReactDOM.render(<VideoListEntry/>, document.getElementById('video-list'));
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+//console.log('obj',exampleVideoData)
+//console.log('snip',exampleVideoData[0].snippet)
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,32 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    { console.log('this', props) }
+    {props.videos.map(el => <VideoListEntry vid = {el.snippet}/> )}
   </div>
 );
+
+// class VideoList extends React.component { 
+//   constuctor(props) { 
+//     super(props);
+//   console.log('2'this.props)
+//   }
+//   render() {
+  
+//     return (
+  
+//     <div className="video-list">
+    
+    
+//     </div>
+//     );
+//   }
+    
+// }
+
+
+
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +37,6 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+//console.log(exampleVideoData)
+
+//
